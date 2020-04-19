5b90dc4beeb7bfd87a02fa5bf7bce8b272bb76a6
miteshpassi <mitesh.passi@gmail.com>
2020-04-19 13:05:55 +0530

    Initial commit

---
+++ src/test/java/guru/springframework/recipe/springrecipe/SpringRecipeApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.recipe.springrecipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRecipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
